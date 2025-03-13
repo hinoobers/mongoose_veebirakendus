bde6f3e9c90b3e2d0991c759371b4094cfcd5173
reio <reio.poldmaa@voco.ee>
2025-03-13 13:20:06 +0200

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import commentController from "./controllers/commentController";
+import articleController from "./controllers/articleController";
+
+mongoose.connect("mongodb+srv://reiopoldmaa:FcLSNRSk8PpndDpc@cluster0.rap2c.mongodb.net/test");
+const database = mongoose.connection;
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(commentController);
+app.use(articleController);
+
+database.on('error', (error) => {
+    console.log(error)
+})
+  
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
